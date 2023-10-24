d39033802dda243e2c84e431147ef37c3a2591be
chethanavirajini <cheth.virajini@gmail.com>
2023-10-24 23:44:34 +0530

    components & pages

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import '../components/Navbar'
+import Navbar from '../components/Navbar'
+import Showproduct from '../components/Showproduct'
 
 function App() {
   
 
   return (
     <>
-      <h1>Hi home page</h1>
+      <Navbar/>
+      <Showproduct/>
     </>
   )
 }
---
+++ src/components/data.jsx
@@ -0,0 +1,95 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Acer',
+        price: 1500,
+        image: '/assets/Acer.webp',
+      },
+      {
+        id: '2',
+        name: 'Azus',
+        price: 2400,
+        image: '/assets/Azus.jpg',
+      },
+      {
+        id: '3',
+        name: 'Apple iPad',
+        price: 1000,
+        image: '/assets/apple-ipad.jpg',
+      },
+      {
+        id: '4',
+        name: 'Appel iPhone',
+        price: 1200,
+        image: '/assets/apple-iphone-15-pro.jpg',
+      },
+      {
+        id: '5',
+        name: 'Tablet',
+        price: 750,
+        image: '/assets/foldabletab.webp',
+      },
+      {
+        id: '6',
+        name: 'Headphone-white',
+        price: 450,
+        image: '/assets/headphone.avif',
+      },
+      {
+        id: '7',
+        name: 'i-watch-gray',
+        price: 700,
+        image: '/assets/i-watch.webp',
+      },
+      {
+        id: '8',
+        name: 'Lenavo Tablet',
+        price: 800,
+        image: '/assets/Lenovo-Tab-K10-Tablets.jpeg',
+      },
+      {
+        id: '9',
+        name: 'Lenavo Tablet-pro',
+        price: 900,
+        image: '/assets/lenovo-tablet-lenovo-tab-p12-pr0.avif',
+      },
+      {
+        id: '10',
+        name: 'MacBook',
+        price: 2000,
+        image: '/assets/macbook.jpg',
+      },
+      {
+        id: '11',
+        name: 'Appel watch ultra',
+        price: 800,
+        image: '/assets/New-Apple-Watch-Ultra.jpg',
+      },
+      {
+        id: '12',
+        name: 'Nokia 3301',
+        price: 250,
+        image: '/assets/Nokia3301.png',
+      },
+      {
+        id: '13',
+        name: 'HeadPhone-black-wireless',
+        price: 670,
+        image: '/assets/pc-studiopro-black.jpg',
+      },
+      {
+        id: '14',
+        name: 'Headphone-wireless',
+        price: 1000,
+        image: '/assets/principal_2000.jpg',
+      },
+      {
+        id: '15',
+        name: 'Samsung Galaxy',
+        price: 500,
+        image: '/assets/samsung-galaxy.webp',
+      }
+    ],
+  };
+  export default data;
